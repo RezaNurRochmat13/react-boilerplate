7e63215e13b10dbd72d2f86fd4f80f5ba861ed31
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-05-29 14:58:00 +0700

    feat(login): add login page

---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded shadow-md">Login Page</div>
+    </div>
+  );
+};
+
+export default LoginPage;
